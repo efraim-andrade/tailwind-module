2d5b215195ceb5c40ab10c8af7b03fb36377d7b7
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-08-07 00:24:56 -0300

    added some classes to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <h1>Hello Tailwind</h1>
-  )
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+
+      <p>Hello tailwind</p>
+    </div>
+  );
 }
